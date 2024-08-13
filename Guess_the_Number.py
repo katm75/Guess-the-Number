e4fcdf9185028ed128babd7898296dec2f45054b
Kathryn <141536562+katm75@users.noreply.github.com>
2024-08-13 16:01:15 -0400

    Fixed the while loop and the infinite loop I was getting thanks to flabdabbet on MeFi

--- Guess_the_Number.py
+++ Guess_the_Number.py
@@ -3,7 +3,7 @@
 #  Kathryn Marks
 #  kathryn.pythonprograms@gmail.com
 #  Created April 26, 2024
-#  Last modified August 12, 2024
+#  Last modified August 13, 2024
 
 
 
@@ -15,7 +15,6 @@
 
 # The random module allows us to generate the mystery number
 
-from math import nextafter
 import random
 
 # Assign a random number to mystery_number
@@ -23,10 +22,10 @@ import random
 mystery_number = random.randint(0,10)
 
 print ("This is a game to guess a random number between 0 and 10" )
-print (mystery_number) # This is for debugging
+# print (mystery_number)  This is for debugging
 
 guess = int(input("What is your first guess?  "))
-print ("You guessed ",guess)
+
 
 # Logic to determine if guess is greater than, less than or equal to mystery_number
 # Need to rewrite this into a while look 
@@ -34,21 +33,21 @@ print ("You guessed ",guess)
     # While guess != mystery_number
         # Test the guess using if elif else
         # break
+                # You don't need to use elif because getting the right answer
+                # breaks you out of the while loop.
 
 
-while guess != mystery_number:      
+while guess != mystery_number:
+    print ("You guessed",guess)
     if guess < mystery_number:
         print ("That's too small, try again")
-    next_guess = int(input("What is your next guess?  "))
-    print ("You guessed ",next_guess)
+    else:
+        print ("That's too big, try again")
+ 
+    guess = int(input("What is your next guess? "))
 
-    elif guess > mystery_number:
-    print ("That's too big, try again")
-    next_guess = int(input("What is your next guess?  "))
-    print ("You guessed ",next_guess)
-    
-    else break
-    print ("Congratuations, you guessed it!")
+print ("Congratulations, you guessed it!")
        
+# Thanks to flabdabbet for helping me understand what I was doing wrong
 
 # EOF
